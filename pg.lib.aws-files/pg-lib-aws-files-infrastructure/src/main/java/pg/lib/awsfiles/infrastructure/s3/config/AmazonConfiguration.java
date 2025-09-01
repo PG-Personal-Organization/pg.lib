7e65a618c2472d7ba54ad9edf02330df15f8938e
Patryk Grzywacz <patrykg198@gmail.com>
2025-09-01 18:47:22 +0200

    fix s3 client

--- pg.lib.aws-files/pg-lib-aws-files-infrastructure/src/main/java/pg/lib/awsfiles/infrastructure/s3/config/AmazonConfiguration.java
+++ pg.lib.aws-files/pg-lib-aws-files-infrastructure/src/main/java/pg/lib/awsfiles/infrastructure/s3/config/AmazonConfiguration.java
@@ -2,6 +2,7 @@ package pg.lib.awsfiles.infrastructure.s3.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -39,7 +40,10 @@ public class AmazonConfiguration {
         return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(Regions.EU_CENTRAL_1)
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                        amazonConfig.getAwsUrl(), Regions.EU_CENTRAL_1.getName()
+                ))
+                .withPathStyleAccessEnabled(Boolean.TRUE)
                 .build();
     }
 }
